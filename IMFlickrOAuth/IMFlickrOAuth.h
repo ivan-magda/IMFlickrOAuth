72fdbf5cbdc8e5726aa2cfe3b36cee94aa4cf313
Ivan Magda <imagda15@gmail.com>
2016-09-02 21:19:03 +0900

    Initial Commit

---
+++ IMFlickrOAuth/IMFlickrOAuth.h
@@ -0,0 +1,19 @@
+//
+//  IMFlickrOAuth.h
+//  IMFlickrOAuth
+//
+//  Created by Ivan Magda on 02/09/16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IMFlickrOAuth.
+FOUNDATION_EXPORT double IMFlickrOAuthVersionNumber;
+
+//! Project version string for IMFlickrOAuth.
+FOUNDATION_EXPORT const unsigned char IMFlickrOAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMFlickrOAuth/PublicHeader.h>
+
+
